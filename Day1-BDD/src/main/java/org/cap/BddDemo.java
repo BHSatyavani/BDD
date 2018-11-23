deb2e366f2821d9382e7214ad71d118db877c4a3
sbairu <sbairu@DIN73003131.corp.capgemini.com>
2018-11-23 17:13:12 +0530

    Hello! This is the second commit

---
+++ Day1-BDD/src/main/java/org/cap/BddDemo.java
@@ -0,0 +1,18 @@
+package org.cap;
+
+public class BddDemo {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Hello");
+			int sum = add(12,13);
+
+		}
+
+		private static int add(int i, int j) {
+			
+			return i+j;
+		}
+
+	}
+
--- Day1-BDD/src/main/java/org/cap/MainClass.java
+++ Day1-BDD/src/main/java/org/cap/MainClass.java
@@ -3,9 +3,15 @@ package org.cap;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Hello Hi");
 
 	}
 
 }
+
+
+
+
+
+	
